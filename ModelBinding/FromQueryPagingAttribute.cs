e30ba325e9ac3e972c6cc9bcf8ecfc4863e660e9
Aren Chen <gucci313031@gmail.com>
2022-10-04 15:54:27 +0800

    移除 PagingBinder

--- ModelBinding/FromQueryPagingAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.AspNetCore.Mvc.ModelBinding;
-
-[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-public class FromQueryPagingAttribute : Attribute, IBindingSourceMetadata
-{
-    public BindingSource BindingSource { get; } = PagingBindingSource.FromQueryPaging;
-}
